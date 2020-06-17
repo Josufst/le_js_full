332c1c1582d3be781c9d0fb754186810309afbb1
Joost van der Linden <gjosuf@gmail.com>
2020-06-17 19:36:47 +0200

    Finish the JavaScript gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,27 @@
 function activateGallery() {
-  alert("Hello from the gallery file!");
+  let thumbnails = document.querySelector("#gallery-thumbs").
+                            querySelectorAll("img");
+  let mainImage = document.querySelector("#gallery-photo img");
+
+  thumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener("click", function() {
+      // Set clicked image as main image.
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      mainImage.setAttribute("src", newImageSrc);	//dit drukt dus de bestaande src weg met de grotere versie van de kleine tumbnail terwijl de CSS hetzelfde blijft.
+      mainImage.setAttribute("alt", thumbnail.alt);
+
+      // Change which image is current.
+      let currentClass = "current"
+      document.querySelector(".current").classList.remove(currentClass);
+      thumbnail.parentNode.classList.add(currentClass);
+
+      // Update image info.
+      let galleryInfo = document.querySelector("#gallery-info");
+      let title       = galleryInfo.querySelector(".title");
+      let description = galleryInfo.querySelector(".description");
+
+      title.innerHTML       = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
+    });
+  });
 }
